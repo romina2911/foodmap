4a3d55dffdfc0fe4936dfc4e9e08ffa16959076c
romina2911 <romina.hp@gmail.com>
2017-12-30 16:38:35 -0500

    subiendo a repositorio remoto

---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    setTimeout(function() {
+      window.location.href = 'views/home.html';
+    }, 4000);
+  });
+  
---
+++ js/data.js
@@ -0,0 +1,130 @@
+var data = {
+    'Japanese Food': {
+    'restaurant': [
+      {
+        'name': 'oishi sushi',
+        'photo': 'https://cdn.cheapoguides.com/wp-content/uploads/sites/2/2015/04/zanmaisushi1-1024x768-1-770x578.jpg',
+        'district': 'San Borja',
+        'stars': 4,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Asuka',
+        'photo': 'https://i.pinimg.com/originals/41/bf/8d/41bf8d19088fc67c23e511edab243622.jpg',
+        'district': 'San Isidro',
+        'stars': 5,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Ramen Express',
+        'photo': 'https://i.pinimg.com/originals/1a/d0/48/1ad0485bbab8c6232891fd74c6304949.jpg',
+        'district': 'Surco',
+        'stars': 5,
+        'price':'$$$'
+      },
+      {
+        'name': 'Maneki Neko',
+        'photo': 'http://www.coolenevada.com/images/Authentic-Romantic-Japanese-Restaurant-Interior-Design-of-Benihana-Las-Vegas-Japanese-Garden.jpg',
+        'district': 'La Molina',
+        'stars': 5,
+        'price':'$$$$$'
+      },
+    ],
+  },
+  'Thai Food': {
+    'restaurant': [
+      {
+        'name': 'Bangkok',
+        'photo': 'http://2.bp.blogspot.com/-zXw_8TX6xiA/UStqr-dgSZI/AAAAAAAABKs/-eBQ3Vg2Xkg/s1600/mullika4.jpg',
+        'district': 'Lince',
+        'stars': 4,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Flavors of Thailand',
+        'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/02/34/2a/1e/sarova-stanley-thai-chi.jpg',
+        'district': 'San Isidro',
+        'stars': 5,
+        'price':'$$$$$'
+      },
+      {
+        'name': 'Thai Kay',
+        'photo': 'http://barcelonapaseodegracia.com/wp-content/uploads/2017/11/Restaurant-Thai-Barcelona.jpg',
+        'district': 'Barranco',
+        'stars': 4,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Glow',
+        'photo': 'http://3.bp.blogspot.com/-higIOqyGpL8/UkidOQr7WZI/AAAAAAAAKpk/Tdp3s3Is9KI/s1600/GlowThai-1006.jpg',
+        'district': 'San Miguel',
+        'stars': 4,
+        'price':'$$$$'
+      },
+    ],
+  },
+  'Mexican Food': {
+    'restaurant': [
+      {
+        'name': 'Pancho Fierro',
+        'photo': 'https://media-cdn.tripadvisor.com/media/photo-s/05/52/bc/bd/family-mexican-restaurant.jpg',
+        'district': 'San Miguel',
+        'stars': 4,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Paquita la del Barrio',
+        'photo': 'http://www.unpublishedarticles.com/wp-content/uploads/2013/12/Mexican-Restaurant.jpg',
+        'district': 'Comas',
+        'stars': 5,
+        'price':'$$$'
+      },
+      {
+        'name': 'Kahlo',
+        'photo': 'https://i1.wp.com/contactohoy.com.mx/wp-content/uploads/2017/03/Tacology-el-restaurante-de-tacos-min.png?fit=1000%2C667&ssl=1',
+        'district': 'Barranco',
+        'stars': 5,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Fresh Burrito',
+        'photo': 'http://www.me3p-france.fr/photo/gallerylist/53/img_3263.jpg',
+        'district': 'San Miguel',
+        'stars': 4,
+        'price':'$$$$'
+      },
+    ],
+ },
+ 'Peruvian Food': {
+    'restaurant': [
+      {
+        'name': 'Don Bosco',
+        'photo': 'https://jmdistrito.files.wordpress.com/2012/10/dscn9347.jpg',
+        'district': 'Jesus Maria',
+        'stars': 4,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Punto Azul',
+        'photo': 'http://www.netlima.com/peru/7299.jpg',
+        'district': 'San Isidro',
+        'stars': 4,
+        'price':'$$$$$'
+      },
+      {
+        'name': 'Chepita Royal',
+        'photo': 'https://farm4.static.flickr.com/3575/3387128739_ce2a3aa5a6.jpg',
+        'district': 'San Miguel',
+        'stars': 4,
+        'price':'$$$$'
+      },
+      {
+        'name': 'Don Tito',
+        'photo': 'https://sanborjavive2013.files.wordpress.com/2013/06/don-tito.jpg',
+        'district': 'San Borja',
+        'stars': 3,
+        'price':'$$$'
+      },
+    ],
+  },
+ };
